3d0c89b28236ea5cb2456a236ef6b5fdc5272a98
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-07-14 10:35:48 +0100

    fixed database

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,15 @@
 const path 		= require('path')
 const sqlite3 	= require('sqlite3').verbose();
-const db_path   = path.resolve(__dirname, 'test.sqlite')
+const db        = `./test.sqlite`;
 
 // note: test.db is in the same directory as connection
 // create connection chain
-let db = new sqlite3.Database(db_path, sqlite3.OPEN_READWRITE, (err) => {
+let db = new sqlite3.Database(db, (err) => {
 	if (err) {
 		console.error(err.message);
-		console.log('Error connecting to database => test.sqlite');
+		console.log(`Error connecting to database => ${db}`);
 	}else{
-		console.log('Connected to the database => test.sqlite');
+		console.log(`Connected to the database => ${db}`);
 	}
 });
 
