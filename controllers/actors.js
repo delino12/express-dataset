c3153207a9f0fb7e231c4330cf7ed60c077fdf94
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-07-14 10:45:06 +0100

    bug fixes

--- controllers/actors.js
+++ controllers/actors.js
@@ -2,20 +2,14 @@ const Actor = require('../models/actor');
 
 var fetchAll = async (req, res) => {
 	await Actor.getAllActors(req, res);
-	
-	res.status(200).json(actors);
 }
 
 var updateActorProfile = async (req, res) => {
 	await Actor.updateActorProfile(req, res);
-	
-	res.status(200).json(actors);
 }
 
 var getActorStreak = async (req, res) => {
 	await Actor.getActorStreak(req, res);
-	
-	res.status(200).json(actors);
 }
 
 module.exports = {
--- database/connection.js
+++ database/connection.js
@@ -1,10 +1,10 @@
 const path 		= require('path')
 const sqlite3 	= require('sqlite3').verbose();
-const db        = `./test.sqlite`;
+const db_path   = `./test.sqlite`;
 
 // note: test.db is in the same directory as connection
 // create connection chain
-let db = new sqlite3.Database(db, (err) => {
+let db = new sqlite3.Database(db_path, (err) => {
 	if (err) {
 		console.error(err.message);
 		console.log(`Error connecting to database => ${db}`);
