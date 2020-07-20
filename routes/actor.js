13f4b1b01c80b9627e014f28faddf57cd52f1f2f
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-07-20 23:30:25 +0100

    update actor route

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,11 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const { getAllActors, getStreak, updateActor } = require('../controllers/actors');
+
+const router = express.Router();
 
 // Routes related to actor.
+router.get('/', getAllActors);
+router.put('/', updateActor);
+router.get('/streak', getStreak);
 
 module.exports = router;
