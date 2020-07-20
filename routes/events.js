85fd5e034b6d7fc433bcd9b37b496b7bfd600cbe
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-07-20 23:31:23 +0100

    update event routes

--- routes/events.js
+++ routes/events.js
@@ -1,7 +1,13 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+const {
+  getAllEvents, addEvent, getByActor,
+} = require('../controllers/events');
 
 // Routes related to event
-
+router.get('/', getAllEvents);
+router.post('/', addEvent);
+router.get('/actors/:id', getByActor);
 
 module.exports = router;
