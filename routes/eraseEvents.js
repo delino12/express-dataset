54db649f7dac3580038cda9737bb8f9d8bbf7323
Ekpoto Liberty Bernard <ekpoto.liberty@gmail.com>
2020-07-20 23:30:51 +0100

    update erase event route

--- routes/eraseEvents.js
+++ routes/eraseEvents.js
@@ -1,6 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
 
+const router = express.Router();
+const { deleteEvents } = require('../controllers/events');
 // Route related to delete events
+router.delete('/', deleteEvents);
 
 module.exports = router;
